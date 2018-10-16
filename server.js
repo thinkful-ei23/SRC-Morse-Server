dba27f6a9b3e05f7ee4c2098a311ada31abaa973
Shelly Leung <pontificable@yahoo.com>
2018-10-16 10:15:18 -0700

    changed question format

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -18,15 +17,15 @@ mongoose.Promise = global.Promise;
 const app = express();
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
-  })
+	morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+		skip: (req, res) => process.env.NODE_ENV === 'test'
+	})
 );
 
 app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
+	cors({
+		origin: CLIENT_ORIGIN
+	})
 );
 
 app.use(express.json());
@@ -38,36 +37,36 @@ app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
 
 app.use((err, req, res, next) => {
-  if (err.status) {
-    const errBody = Object.assign({}, err, { message: err.message });
-    res.status(err.status).json(errBody);
-  } else {
-    console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
+	if (err.status) {
+		const errBody = Object.assign({}, err, { message: err.message });
+		res.status(err.status).json(errBody);
+	} else {
+		console.error(err);
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
 });
 
 if (process.env.NODE_ENV !== 'test') {
-  mongoose
-    .connect(MONGODB_URI)
-    .then(instance => {
-      const conn = instance.connections[0];
-      console.info(
-        `Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`
-      );
-    })
-    .catch(err => {
-      console.error(`ERROR: ${err.message}`);
-      console.error('\n === Did you remember to start `mongod`? === \n');
-      console.error(err);
-    });
-  app
-    .listen(PORT, function () {
-      console.info(`Server listening on ${this.address().port}`);
-    })
-    .on('error', err => {
-      console.error(err);
-    });
+	mongoose
+		.connect(MONGODB_URI)
+		.then(instance => {
+			const conn = instance.connections[0];
+			console.info(
+				`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`
+			);
+		})
+		.catch(err => {
+			console.error(`ERROR: ${err.message}`);
+			console.error('\n === Did you remember to start `mongod`? === \n');
+			console.error(err);
+		});
+	app
+		.listen(PORT, function() {
+			console.info(`Server listening on ${this.address().port}`);
+		})
+		.on('error', err => {
+			console.error(err);
+		});
 }
 
 module.exports = app;
